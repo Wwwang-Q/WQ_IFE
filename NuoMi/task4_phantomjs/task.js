74c52eec9abe77b3b72da3c2e32aeb6618502353
Wwwang-Q <wangqi@wangqideMBP.lan>
2017-03-02 17:04:40 +0800

    Phantomjs_1

---
+++ NuoMi/task4_phantomjs/task.js
@@ -0,0 +1,28 @@
+/**
+ * Created by wangqi on 2017/3/2.
+ */
+var page =require('webpage').create();
+var system=require('system');
+var url="https://www.baidu.com";
+var content;
+
+page.open(url,function (status) {
+    if (status!=="success"){
+        console.log("Fail to render!");
+    }else {
+
+             content=page.evaluate(function (){
+                var keyWord=document.getElementById('kw');
+                var btn=document.getElementById('su');
+                keyWord.value="key";
+                btn.click();
+                return keyWord.value;
+            });
+
+        window.setTimeout(function () {
+            page.render("test.png");
+            phantom.exit();
+        },3000);
+    };
+
+})
