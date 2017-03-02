32c51a3a8697e3cc49a6dee4bec8bd29b86b4d9e
Wwwang-Q <wangqi@wangqideMacBook-Pro.local>
2017-03-02 23:58:20 +0800

    update

--- NuoMi/task4_phantomjs/task.js
+++ NuoMi/task4_phantomjs/task.js
@@ -1,28 +1,58 @@
 /**
  * Created by wangqi on 2017/3/2.
  */
+
+//phantom.outputEncoding = 'gbk';
 var page =require('webpage').create();
 var system=require('system');
-var url="https://www.baidu.com";
-var content;
+var fs=require('fs');
+var key=system.args[1];
+var url="https://www.baidu.com/s?wd="+encodeURI(key);
+var time=Date.now();
+var data={};
+// 获取网页控制台的 console.log() 内容
 
 page.open(url,function (status) {
     if (status!=="success"){
-        console.log("Fail to render!");
+        time = Date.now() - time;
+        data={
+            code:0,
+            msg:'抓取失败',
+            word:key,
+            time:time
+        };
+        data=JSON.stringify(data,null,4);
+        console.log(data);
+        fs.write("task.json",data,'w');
+        phantom.exit();
     }else {
+        page.includeJs("https://code.jquery.com/jquery-3.1.1.min.js",function () {
+            data=page.evaluate(function (time,key){
+              var tmp=[];
+               var total=$('.c-container');
+               for(var i=0;i<total.length;i++){
+                   var list={};
+                   list.title=$(total[i]).find('.t').text().trim();
+                   list.info=$(total[i]).find('.c-abstract').text();
+                   list.link=$(total[i]).find('.t a').attr('href');
+                   var pic=$(total[i]).find('c-img');
+                   list.pic=pic.length?pic.attr('scr'):'';
+                   tmp.push(list);
+               }
 
-             content=page.evaluate(function (){
-                var keyWord=document.getElementById('kw');
-                var btn=document.getElementById('su');
-                keyWord.value="key";
-                btn.click();
-                return keyWord.value;
-            });
+                return JSON.stringify({
+                    code:1,
+                    msg:'抓取成功',
+                    word:key,
+                    time:time,
+                    dataList:tmp
+                },null,4);
 
-        window.setTimeout(function () {
-            page.render("test.png");
+            },time,key);
+            fs.write("task.json",data,'w');
+            console.log(data);
             phantom.exit();
-        },3000);
-    };
+        });
+    }
 
-})
+});
